952851b3dfa81999c60050d5656184ae0a5c2c97
frkri <57220519+frkri@users.noreply.github.com>
2024-07-01 12:47:49 +0000

    Send correct headers

--- src/routes/key/+server.ts
+++ src/routes/key/+server.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from './$types';
 import key from '$lib/assets/key/frkri.asc?raw';
 
 export const GET: RequestHandler = async () => {
-	return new Response(key);
+	return new Response(key, { headers: { 'Content-Type': 'text/plain' } });
 };
