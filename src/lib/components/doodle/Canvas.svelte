34fd5e4f32b3a59e925b5cc77f2a1305b37ff951
frkri <hello@frkri.dev>
2024-07-07 00:21:52 +0200

    Improve canvas cursor

--- src/lib/components/doodle/Canvas.svelte
+++ src/lib/components/doodle/Canvas.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
 	import { CanvasMode } from '$lib/types/doodle';
+	const MAX_RADIUS = 10;
+	const DEFAULT_RADIUS = MAX_RADIUS / 2;
+	const MIN_RADIUS = 1;
 
 	let {
 		mode = $bindable(CanvasMode.IDLE),
 		color = $bindable('#FFFFFF')
 	}: { mode: CanvasMode; color: string } = $props();
 
-	let cursor = $derived.by(() => {
-		switch (mode) {
-			case CanvasMode.DRAW:
-				return 'crosshair';
-			default:
-				return 'default';
+	let cursorRadius = $state(DEFAULT_RADIUS);
+	let cursorStyle = $derived.by(() => {
+		if (mode === CanvasMode.DRAW) {
+			return `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='${MAX_RADIUS * 2}' height='${MAX_RADIUS * 2}' fill='${color.replace('#', '%23')}AA'><circle cx='${(MAX_RADIUS * 2) / 2}' cy='${(MAX_RADIUS * 2) / 2}' r='${cursorRadius}'></circle></svg>") 16 16, auto`;
+		} else {
+			return 'default';
 		}
 	});
+
+	function handleKey(e: KeyboardEvent) {
+		if (e.ctrlKey) return;
+		switch (e.key) {
+			case '+':
+				cursorRadius = Math.min(cursorRadius + 1, MAX_RADIUS);
+				break;
+			case '-':
+				cursorRadius = Math.max(cursorRadius - 1, MIN_RADIUS);
+				break;
+			case '=':
+				cursorRadius = DEFAULT_RADIUS;
+				break;
+		}
+	}
 </script>
 
+<svelte:window on:keydown={handleKey} />
 {#if mode !== CanvasMode.IDLE}
-	<canvas style="cursor: {cursor};"></canvas>
+	<canvas style="cursor: {cursorStyle};"></canvas>
 {/if}
 
 <style>
