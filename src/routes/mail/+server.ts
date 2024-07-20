f5494bbec91d5d412f02f2dc632e11fdad598256
frkri <hello@frkri.dev>
2024-07-20 02:19:09 +0200

    Improve mail route

--- src/routes/mail/+server.ts
+++ src/routes/mail/+server.ts
@@ -1,15 +1,19 @@
 import { env } from '$env/dynamic/private';
 import type { RequestHandler } from './$types';
 
+const goodUA = ['Mozilla', 'Safari', 'Chrome'];
+
 export const GET: RequestHandler = async ({ request }) => {
 	const userAgent = request.headers.get('User-Agent');
-	const goodUA = ['Mozilla', 'Safari', 'Chrome'];
 
 	if (!userAgent || !goodUA.some((ua) => userAgent.includes(ua)))
 		return new Response('Sorry, this is not for you. 🤖', { status: 403 });
 
-	return new Response('Hello, friend! 👋', {
-		status: 301,
-		headers: { Location: 'mailto:' + env.EMAIL }
-	});
+	return new Response(
+		'Hello, friend! 👋\nYour email client should open soon. If not, try refreshing the page.',
+		{
+			status: 301,
+			headers: { Location: 'mailto:' + env.EMAIL }
+		}
+	);
 };
